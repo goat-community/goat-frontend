040f8d3dbe396f24e288779f224b832603c7a697
Ebubeker <rexhaebubeker@gmail.comm>
2023-09-08 17:55:54 +0200

    started moving the components to override folder of theme so that we avoid creating costum atomic components

---
+++ packages/ui/stories/components/Card.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ThemeProvider from "../../theme/ThemeProvider";
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import { useDarkMode } from "storybook-dark-mode";
+import { Icon, ICON_NAME } from "../../components/Icon";
+
+const meta: Meta<typeof Card> = {
+  component: Card,
+  tags: ["autodocs"],
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <ThemeProvider
+        settings={{
+          themeColor: "primary",
+          contentWidth: "boxed",
+          mode: useDarkMode() ? "dark" : "light",
+        }}
+      >
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const Default: Story = {
+  args: {
+    children: "Card",
+  },
+  render: (args) => {
+    const { children, ...rest } = args;
+    return (
+      <Card sx={{ width: "280px" }}>
+        <CardMedia
+          sx={{ height: 100 }}
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQy9x3wyV5OWYWA8XxBJKMlH2QvuSSOIdOItRK1jgXSQ&s"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography variant="body2" color="secondary">
+            Test Body
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/tu6pPILBRSUuy3Hbu8Lphk/Root_Goat-3.0?type=design&node-id=6583-46474&mode=design&t=9dTs5ps2RVfIB1th-0",
+    },
+  },
+};
--- packages/ui/theme/overrides/index.ts
+++ packages/ui/theme/overrides/index.ts
@@ -2,10 +2,13 @@ import { Theme } from "@mui/material/styles";
 
 import MuiAvatar from "./avatar";
 import Button from "./button";
+import Card from "./card";
+
 const Overrides = (theme: Theme) => {
   return {
     ...MuiAvatar(theme),
     ...Button(theme),
+    ...Card(theme),
   };
 };
 
