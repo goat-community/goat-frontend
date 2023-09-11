9c34444bb890fd9f3023d2aa4a2c2eee05675b1f
Majk Shkurti <majkshkurti@outlook.com>
2023-09-11 21:36:15 +0200

    fix

--- packages/keycloak-theme/src/login/Template.tsx
+++ packages/keycloak-theme/src/login/Template.tsx
@@ -34,183 +34,184 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
     currentLanguageTag,
   } = i18n;
   return (
-    <div>
-      <Box
-        component="main"
+    <Box
+      component="main"
+      sx={{
+        display: "flex",
+        flex: "1 1 auto",
+      }}
+    >
+      <Grid
+        container
         sx={{
-          display: "flex",
           flex: "1 1 auto",
         }}
       >
         <Grid
-          container
+          xs={12}
+          lg={6}
           sx={{
-            flex: "1 1 auto",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            position: 'relative'
           }}
         >
-          <Grid
-            xs={12}
-            lg={6}
+          <Box
+            component="header"
             sx={{
-              height: "100vh",
-              display: "flex",
-              flexDirection: "column",
+              left: 0,
+              p: 3,
+              position: "fixed",
+              top: 0,
+              width: "100%",
             }}
           >
             <Box
-              component="header"
+              component="a"
+              href="https://www.plan4better.de/"
+              target="_blank"
               sx={{
-                left: 0,
-                p: 3,
-                position: "fixed",
-                top: 0,
-                width: "100%",
+                display: "inline-flex",
+                width: 160,
               }}
             >
-              <Box
-                component="a"
-                href="https://www.plan4better.de/"
-                target="_blank"
-                sx={{
-                  display: "inline-flex",
-                  width: 160,
-                }}
-              >
-                <img
-                  width="100%"
-                  src={`https://assets.plan4better.de/img/logo/plan4better_${
-                    theme.palette.mode === "light" ? "standard" : "white"
-                  }.svg`}
-                  alt="Plan4Better Logo"
-                />
-              </Box>
-            </Box>
-            <Box
-              component="div"
-              sx={{
-                flex: "1 1 auto",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Card
-                sx={{
-                  width: 490,
-                  height: "fit-content",
-                  marginBottom: theme.spacing(4),
-                }}
-              >
-                <CardContent>
-                  {/* Header */}
-                  <Stack
-                    spacing={theme.spacing(4)}
-                    sx={{
-                      mb: theme.spacing(4),
-                      textAlign: "center",
-                    }}
-                  >
-                    <Typography
-                      variant="h5"
-                      id="kc-page-title"
-                      sx={{
-                        pb: theme.spacing(4),
-                      }}
-                    >
-                      {headerNode}
-                    </Typography>
-                    {displayMessage &&
-                      message !== undefined &&
-                      (message.type !== "warning" || !isAppInitiatedAction) && (
-                        <Alert severity={message.type}>{message.summary}</Alert>
-                      )}
-                  </Stack>
-                  {/* Pages */}
-                  {children}
-                  {/* Footer */}
-                  {displayInfo && (
-                    <Stack
-                      spacing={theme.spacing(2)}
-                      sx={{
-                        mt: theme.spacing(4),
-                        textAlign: "left",
-                      }}
-                    >
-                      {infoNode}
-                    </Stack>
-                  )}
-                </CardContent>
-                {/* Language and Theme Toggle Mode */}
-                {["register-user-profile.ftl", "login.ftl"].includes(
-                  kcContext.pageId,
-                ) && (
-                  <Stack
-                    justifyContent="flex-end"
-                    direction="row"
-                    spacing={theme.spacing(2)}
-                    sx={{
-                      pb: theme.spacing(2),
-                      px: theme.spacing(4),
-                    }}
-                  >
-                    {realm.internationalizationEnabled &&
-                      locale &&
-                      locale.supported.length > 1 && (
-                        <LanguageDropdown
-                          toolTipProps={{
-                            title: msgStr("changeLanguage"),
-                            placement: "top",
-                          }}
-                          languages={labelBySupportedLanguageTag}
-                          selected={currentLanguageTag}
-                          onLanguageChange={changeLocale}
-                        />
-                      )}
-                    <DarkModeSwitch
-                      sx={{
-                        ml: theme.spacing(2),
-                      }}
-                      size="small"
-                      onClick={() => {
-                        colorMode.toggleColorMode();
-                      }}
-                      toolTipProps={{
-                        title: msgStr("changeTheme"),
-                        placement: "top",
-                      }}
-                      isDarkModeEnabled={theme.palette.mode === "dark"}
-                    />
-                  </Stack>
-                )}
-              </Card>
-            </Box>
-          </Grid>
-          <Grid
-            xs={12}
-            lg={6}
-            sx={{
-              alignItems: "center",
-              background:
-                "radial-gradient(50% 50% at 50% 50%, rgba(40,54,72,0.8) 0%, rgba(40,54,72,0.9) 100%), url(https://assets.plan4better.de/img/login/artwork_1.png) no-repeat center",
-              backgroundSize: "cover",
-              display: "flex",
-              justifyContent: "center",
-              "& img": {
-                maxWidth: "100%",
-              },
-            }}
-          >
-            <Box sx={{ p: 3, width: 350 }} component="div">
               <img
                 width="100%"
-                src="https://assets.plan4better.de/img/logo/goat_white.svg"
+                src={`https://assets.plan4better.de/img/logo/plan4better_${
+                  theme.palette.mode === "light" ? "standard" : "white"
+                }.svg`}
                 alt="Plan4Better Logo"
               />
             </Box>
-          </Grid>
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              flex: "1 1 auto",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Card
+              sx={{
+                width: 490,
+                height: "fit-content",
+                marginBottom: theme.spacing(4),
+              }}
+            >
+              <CardContent>
+                {/* Header */}
+                <Stack
+                  spacing={theme.spacing(4)}
+                  sx={{
+                    mb: theme.spacing(4),
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    id="kc-page-title"
+                    sx={{
+                      pb: theme.spacing(4),
+                    }}
+                  >
+                    {headerNode}
+                  </Typography>
+                  {displayMessage &&
+                    message !== undefined &&
+                    (message.type !== "warning" || !isAppInitiatedAction) && (
+                      <Alert severity={message.type}>{message.summary}</Alert>
+                    )}
+                </Stack>
+                {/* Pages */}
+                {children}
+                {/* Footer */}
+                {displayInfo && (
+                  <Stack
+                    spacing={theme.spacing(2)}
+                    sx={{
+                      mt: theme.spacing(4),
+                      textAlign: "left",
+                    }}
+                  >
+                    {infoNode}
+                  </Stack>
+                )}
+              </CardContent>
+              {/* Language and Theme Toggle Mode */}
+              {[
+                "register-user-profile.ftl",
+                "login.ftl",
+                "login-username.ftl",
+              ].includes(kcContext.pageId) && (
+                <Stack
+                  justifyContent="flex-end"
+                  direction="row"
+                  spacing={theme.spacing(2)}
+                  sx={{
+                    pb: theme.spacing(2),
+                    px: theme.spacing(4),
+                  }}
+                >
+                  {realm.internationalizationEnabled &&
+                    locale &&
+                    locale.supported.length > 1 && (
+                      <LanguageDropdown
+                        toolTipProps={{
+                          title: msgStr("changeLanguage"),
+                          placement: "top",
+                        }}
+                        languages={labelBySupportedLanguageTag}
+                        selected={currentLanguageTag}
+                        onLanguageChange={changeLocale}
+                      />
+                    )}
+                  <DarkModeSwitch
+                    sx={{
+                      ml: theme.spacing(2),
+                    }}
+                    size="small"
+                    onClick={() => {
+                      colorMode.toggleColorMode();
+                    }}
+                    toolTipProps={{
+                      title: msgStr("changeTheme"),
+                      placement: "top",
+                    }}
+                    isDarkModeEnabled={theme.palette.mode === "dark"}
+                  />
+                </Stack>
+              )}
+            </Card>
+          </Box>
         </Grid>
-      </Box>
-    </div>
+        <Grid
+          xs={12}
+          lg={6}
+          sx={{
+            alignItems: "center",
+            background:
+              "radial-gradient(50% 50% at 50% 50%, rgba(40,54,72,0.8) 0%, rgba(40,54,72,0.9) 100%), url(https://assets.plan4better.de/img/login/artwork_1.png) no-repeat center",
+            backgroundSize: "cover",
+            display: "flex",
+            justifyContent: "center",
+            "& img": {
+              maxWidth: "100%",
+            },
+          }}
+        >
+          <Box sx={{ p: 3, width: 350 }} component="div">
+            <img
+              width="100%"
+              src="https://assets.plan4better.de/img/logo/goat_white.svg"
+              alt="Plan4Better Logo"
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
