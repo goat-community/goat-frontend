32ca9cdee2082e63dd3403fa35935f460e75acb2
Majk Shkurti <majkshkurti@outlook.com>
2024-07-03 15:27:44 +0200

    fix: dataset_table json viewer, scenario coming_soon

--- apps/web/components/modals/DatasetTable.tsx
+++ apps/web/components/modals/DatasetTable.tsx
@@ -77,9 +77,19 @@ const Row = ({ row, fields }) => {
               <Box sx={{ margin: 2 }}>
                 {objectFields.map((field) => {
                   const jsonData = JSON.parse(row.properties[field.name]);
+                  const isJsonDataArrayOfObjects =
+                    Array.isArray(jsonData) &&
+                    jsonData.length > 0 &&
+                    typeof jsonData[0] === "object" &&
+                    !Array.isArray(jsonData[0]);
+
                   return (
                     <>
-                      <Stack direction="column" spacing={1} sx={{ py: 1, pl: 4 }}>
+                      <Stack
+                        direction="column"
+                        spacing={1}
+                        sx={{ py: 1, pl: 4 }}
+                      >
                         <Typography variant="body2" fontWeight="bold">
                           {field.name}
                         </Typography>
@@ -87,29 +97,38 @@ const Row = ({ row, fields }) => {
                           {field.type}
                         </FieldTypeTag>
                       </Stack>
-                      <Table
-                        size="small"
-                        aria-label="purchases"
-                        key={field.name}
-                      >
-                        <TableHead>
-                          <TableRow>
-                            {jsonData.length > 0 &&
-                              Object.keys(jsonData[0]).map((key) => (
+                      {isJsonDataArrayOfObjects ? (
+                        <Table
+                          size="small"
+                          aria-label="purchases"
+                          key={field.name}
+                        >
+                          <TableHead>
+                            <TableRow>
+                              {Object.keys(jsonData[0]).map((key) => (
                                 <TableCell key={key}>{key}</TableCell>
                               ))}
-                          </TableRow>
-                        </TableHead>
-                        <TableBody>
-                          {jsonData.map((item, rowIndex) => (
-                            <TableRow key={rowIndex}>
-                              {Object.values(item).map((value: string, cellIndex) => (
-                                <TableCell key={cellIndex}>{value}</TableCell>
-                              ))}
                             </TableRow>
-                          ))}
-                        </TableBody>
-                      </Table>
+                          </TableHead>
+                          <TableBody>
+                            {jsonData.map((item, rowIndex) => (
+                              <TableRow key={rowIndex}>
+                                {Object.values(item).map(
+                                  (value: string, cellIndex) => (
+                                    <TableCell key={cellIndex}>
+                                      {value.toString()}
+                                    </TableCell>
+                                  ),
+                                )}
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      ) : (
+                        // Handle the case where jsonData is not an array of objects
+                        // This could be rendering it as a string or handling other data structures in the future.
+                        <Typography>{JSON.stringify(jsonData, null, 2)}</Typography>
+                      )}
                     </>
                   );
                 })}
@@ -144,8 +163,6 @@ const DatasetTableModal: React.FC<DatasetTableDialogProps> = ({
     dataset["layer_id"] || dataset["id"] || "",
     dataQueryParams,
   );
-
-  console.log(data);
 
   const [displayData, setDisplayData] = useState(data);
   useEffect(() => {
