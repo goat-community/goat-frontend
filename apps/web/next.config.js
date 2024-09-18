0bd9dcbef09050bbc3d3fec46f3ba66da2538e0c
majkshkurti <majkshkurti@outlook.com>
2024-09-18 16:31:07 +0200

    feat: add sentry monitoring

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,9 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withBundleAnalyzer({
+const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   transpilePackages: ["@p4b/ui", "@p4b/tsconfig"],
@@ -14,4 +15,27 @@ module.exports = withBundleAnalyzer({
   images: {
     domains: ["assets.plan4better.de", "source.unsplash.com"],
   },
-});
+};
+
+const sentryConfig = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+  org: "plan4better",
+  project: "goat-frontend",
+  // Suppresses source map uploading logs during build
+  silent: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+  tunnelRoute: "/api/monitoring",
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+};
+
+const finalConfig = process.env.NEXT_PUBLIC_SENTRY_DSN
+  ? withSentryConfig(nextConfig, sentryConfig)
+  : nextConfig;
+
+module.exports = withBundleAnalyzer(finalConfig);
